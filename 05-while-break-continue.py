e9211dbac96ab3fbb5441ed632a1726f51530b92
AevumElectronics <aevumelectronics@gmail.com>
2021-12-25 18:13:20 +0100

    while-break-continue

--- 05-while-break-continue.py
+++ 05-while-break-continue.py
@@ -11,13 +11,18 @@ def main():
     while True:
         print('Do you want to exit the loop?')
         answer = input()
-        if answer != 'yes':
-            continue
-        print('What do we say?')
+        if answer != 'yes':  # if your answer is not "yes" you will enter the "if" statement
+            continue         # the "continue" statement immediately restart the loop
+        print('What do we say?')    # be gentle
         gentile = input()
         if gentile == 'please':
-            break
+            break   # if you want to exit the "while" loop you will need to say please
     print('Ok...Now you are out of the loop, bye!')
+
+# instead of waiting for the loop to finish, we use the "continue" statement to halve the code inside the loop
+# is useless to do stuff if the condition aren't met, so is better to restart.
+# the "while" loop goes on forever, the "break" is reachable only if we met all the conditions,
+# and it's the only way of getting out of the loop.
 
 # Standard boilerplate to call the main() function to begin the program.
 # copy and paste this stuff everywhere.
