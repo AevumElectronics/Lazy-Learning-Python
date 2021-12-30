ade0c34a82e2b56722152f4e9c232132db440239
AevumElectronics <aevumelectronics@gmail.com>
2021-12-30 20:05:47 +0100

    startup name

--- 21-startup-name.py
+++ 21-startup-name.py
@@ -6,13 +6,16 @@ def main():
     import string
 
     print("Select the number of letter:")
-    num = int(input())
+    num = int(input())  # length of your company name
 
-
-    for i in range(5):
+    for i in range(5):  # modify if you want more than 5 names
         vector = random.choices(string.ascii_uppercase, k=num)
         print('StartUp Name ' + str(i+1) + ': ' + ' '.join(vector))
 
+# random.choices() generate random elements
+# the string.ascii_uppercase return an uppercase ascii character as a string
+# k=num is the parameter we use to select the length of the list
+# the ' '.join(vector) function joins the list as one element
 
 # Standard boilerplate to call the main() function to begin the program.
 # copy and paste this stuff everywhere.
